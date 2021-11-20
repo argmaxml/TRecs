a68615184405fc633da4ab9fa6b2923b192c0f8b
Uri Goren <uri@goren.ml>
2021-11-20 23:47:47 +0200

    query endpoint

--- src/endpoint.py
+++ src/endpoint.py
@@ -31,17 +31,17 @@ async def read_root():
 
 @app.get("/partitions")
 async def api_partitions():
-    return {"partitons": schema["partitions"], "n":len(schema["partitions"])}
+    return {"status":"OK", "partitons": schema["partitions"], "n":len(schema["partitions"])}
 
 @app.post("/encode")
 async def api_encode(data: Dict[str,str]):
     vec = schema["encode_fn"](data)
-    return [float(x) for x in vec]
+    return {"status": "OK", "vec": [float(x) for x in vec]}
 
 @app.post("/index")
 async def api_index(data: List[Dict[str,str]]):
     try:
-        vecs = sorted([(schema["index_num"](datum),schema["encode_fn"](datum), datum["id"]) for datum in data])
+        vecs = sorted([(schema["index_num"](datum),schema["encode_fn"](datum), datum["id"]) for datum in data], key=at(0))
     except KeyError as e:
         return {"status": "error", "message": str(e)}
     affected_partitions = 0
@@ -49,7 +49,24 @@ async def api_index(data: List[Dict[str,str]]):
         _,items,ids = zip(*grp)
         partitions[idx].add_items(items, ids)
         affected_partitions+=1
-    return {"affected_partitions": affected_partitions}
+    return {"status": "OK", "affected_partitions": affected_partitions}
+
+@app.post("/query")
+async def api_query(data: Dict[str,str]):
+    try:
+        idx = schema["index_num"](data)
+    except Exception as e:
+        return {"status": "error", "message": "Error in partitioning: " + str(e)}
+    try:
+        vec = schema["encode_fn"](data)
+    except:
+        return {"status": "error", "message": "Error in encoding: " +  str(e)}
+    #TODO: get k
+    labels, distances = partitions[idx].knn_query(vec, k = 2)
+    return {"status":"OK", "ids": [str(l) for l in labels[0]], "distances": [float(d) for d in distances[0]]}
+
+
+
 
 if __name__=="__main__":
     import uvicorn
