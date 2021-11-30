a549f2cf2422156297659e97e4e0289dc2493499
Uri Goren <uri@goren.ml>
2021-11-30 22:16:19 +0200

    1. PEP8 spacing 2. stupid bugfix

--- src/encoders.py
+++ src/encoders.py
@@ -4,110 +4,131 @@ from operator import itemgetter as at
 import numpy as np
 from tree_helpers import lowest_depth, get_values_nested
 
+
 def parse_schema(schema):
     if hasattr(schema, 'read'):
         schema = schema.read()
-    if type(schema)==str:
+    if type(schema) == str:
         schema = json.loads(schema)
-    assert type(schema)==dict, "Schema type should be a dict"
+    assert type(schema) == dict, "Schema type should be a dict"
     assert "filters" in schema, "filters not in schema"
     assert "encoders" in schema, "encoders not in schema"
     ret = {"metric": schema["metric"]}
     partitions = list(itertools.product(*[f["values"] for f in schema["filters"]]))
     ret["partitions"] = partitions
-    tup = lambda t: t if type(t)==tuple else (t,)
+    tup = lambda t: t if type(t) == tuple else (t,)
     ret["index_num"] = lambda x: partitions.index(tup(at(*[f["field"] for f in schema["filters"]])(x)))
     encoder = dict()
     for enc in schema["encoders"]:
-        if enc["type"] in ["onehot","one_hot", "one hot", "oh"]:
-            encoder[enc["field"]] = OneHotEncoder(column=enc["field"], column_weight=enc["weight"], values=enc["values"])
-        elif enc["type"] in ["ordinal","ordered"]:
-            encoder[enc["field"]] = OrdinalEncoder(column=enc["field"], column_weight=enc["weight"], values=enc["values"], window=enc["window"])
+        if enc["type"] in ["onehot", "one_hot", "one hot", "oh"]:
+            encoder[enc["field"]] = OneHotEncoder(column=enc["field"], column_weight=enc["weight"],
+                                                  values=enc["values"])
+        elif enc["type"] in ["ordinal", "ordered"]:
+            encoder[enc["field"]] = OrdinalEncoder(column=enc["field"], column_weight=enc["weight"],
+                                                   values=enc["values"], window=enc["window"])
         elif enc["type"] in ["bin", "binning"]:
-            encoder[enc["field"]] = BinEncoder(column=enc["field"], column_weight=enc["weight"], values=enc["values"], boundaries=enc["boundaries"])
+            encoder[enc["field"]] = BinEncoder(column=enc["field"], column_weight=enc["weight"], values=enc["values"],
+                                               boundaries=enc["boundaries"])
         elif enc["type"] in ["hier", "hierarchy", "nested"]:
-            encoder[enc["field"]] = HierarchyEncoder(column=enc["field"], column_weight=enc["weight"], values=enc["values"], similarity_by_depth=enc["similarity_by_depth"])
+            encoder[enc["field"]] = HierarchyEncoder(column=enc["field"], column_weight=enc["weight"],
+                                                     values=enc["values"],
+                                                     similarity_by_depth=enc["similarity_by_depth"])
         else:
-            raise TypeError("Unknown type {t} in field {f}".format(f=enc["field"],t=enc["type"]))
-    ret["encoders"]=encoder
-    ret["encode_fn"]=lambda d:np.concatenate([e.encode(d[f]) for f,e in encoder.items()])
-    ret["dim"] = sum(map(len,encoder.values()))
+            raise TypeError("Unknown type {t} in field {f}".format(f=enc["field"], t=enc["type"]))
+    ret["encoders"] = encoder
+    ret["encode_fn"] = lambda d: np.concatenate([e.encode(d[f]) for f, e in encoder.items()])
+    ret["dim"] = sum(map(len, encoder.values()))
     return ret
 
+
 class ColumnEncoder:
-    column=''
-    column_weight=1
+    column = ''
+    column_weight = 1
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
     def __len__(self):
         raise NotImplementedError("len is not implemented")
+
     def encode(self, value):
         return np.array([])
 
+
 class OneHotEncoder(ColumnEncoder):
-    values=['a','b','c']
+    values = ['a', 'b', 'c']
+
     def __len__(self):
-        return len(self.values)+1
+        return len(self.values) + 1
+
     def encode(self, value):
-        vec = np.zeros(1+len(self.values))
+        vec = np.zeros(1 + len(self.values))
         try:
-            vec[1+self.values.index(value)] = 1
-        except ValueError: # Unknown
+            vec[1 + self.values.index(value)] = 1
+        except ValueError:  # Unknown
             vec[0] = 1
         return vec
+
 
 class OrdinalEncoder(ColumnEncoder):
-    values=['a','b','c']
+    values = ['a', 'b', 'c']
+
     def __len__(self):
-        return len(self.values)+1
-    window=[0.5,1,0.5]
+        return len(self.values) + 1
+
+    window = [0.5, 1, 0.5]
+
     def encode(self, value):
-        assert len(window)%1==1, "Window size should be odd"
-        vec = np.zeros(1+len(self.values))
+        assert len(window) % 1 == 1, "Window size should be odd"
+        vec = np.zeros(1 + len(self.values))
         try:
             ind = self.values.index(value)
-        except ValueError: # Unknown
+        except ValueError:  # Unknown
             vec[0] = 1
             return vec
-        vec[1+ind] = window[len(self.window)//2+1]
-        for offset in range(len(self.window)//2):
-            if ind-offset>=0:
-                vec[1+ind-offset] = self.window[len(self.window)//2-offset]
-            if ind+offset<len(self.values):
-                vec[1+ind+offset] = self.window[len(self.window)//2+offset]
+        vec[1 + ind] = window[len(self.window) // 2 + 1]
+        for offset in range(len(self.window) // 2):
+            if ind - offset >= 0:
+                vec[1 + ind - offset] = self.window[len(self.window) // 2 - offset]
+            if ind + offset < len(self.values):
+                vec[1 + ind + offset] = self.window[len(self.window) // 2 + offset]
 
         return vec
+
 
 class BinEncoder(ColumnEncoder):
-    boundaries = [1,2,3]
+    boundaries = [1, 2, 3]
+
     def __len__(self):
-        return len(self.boundaries)+1
+        return len(self.boundaries) + 1
+
     def encode(self, value):
-        vec = np.zeros(2+len(self.boundaries))
+        vec = np.zeros(2 + len(self.boundaries))
         i = 0
-        while i<len(self.boundaries) and value>self.boundaries[i]:
-            i+=1
-        vec[i]=1
+        while i < len(self.boundaries) and value > self.boundaries[i]:
+            i += 1
+        vec[i] = 1
+
 
 class HierarchyEncoder(ColumnEncoder):
-    values={'a':['a1','a2'],'b':['b1', 'b2'],'c':{'c1':['c11','c12']}}
-    similarity_by_depth = [1,0.5,0]
+    values = {'a': ['a1', 'a2'], 'b': ['b1', 'b2'], 'c': {'c1': ['c11', 'c12']}}
+    similarity_by_depth = [1, 0.5, 0]
 
     def __len__(self):
         inner_values = get_values_nested(self.values)
-        return (1+len(inner_values))
+        return (1 + len(inner_values))
+
     def encode(self, value):
-        #TODO: very inefficient: move to constructor
+        # TODO: very inefficient: move to constructor
         inner_values = get_values_nested(self.values)
-        vec = np.zeros(1+len(inner_values))
+        vec = np.zeros(1 + len(inner_values))
         try:
             for other_value in inner_values:
-                depth = lowest_depth(self.values,value,other_value)
-                if depth>=len(self.similarity_by_depth):
+                depth = lowest_depth(self.values, value, other_value)
+                if depth >= len(self.similarity_by_depth):
                     # defaults to zero
                     continue
-                vec[1+inner_values.index(other_value)] = self.similarity_by_depth[depth]
-        except ValueError: # Unknown
+                vec[1 + inner_values.index(other_value)] = self.similarity_by_depth[depth]
+        except ValueError:  # Unknown
             vec[0] = 1
         return vec
-
--- src/endpoint.py
+++ src/endpoint.py
@@ -1,20 +1,23 @@
-from typing import Optional, List, Dict, Any 
+from typing import Optional, List, Dict, Any
 from pydantic import BaseModel, Field
 import numpy as np
 import sys, json, itertools
 from fastapi import FastAPI
 from operator import itemgetter as at
 from pathlib import Path
+
 sys.path.append("../src")
 from hnsw_helpers import LazyHnsw
 import encoders
+
 data_dir = Path(__file__).absolute().parent.parent / "data"
 model_dir = Path(__file__).absolute().parent.parent / "models"
 app = FastAPI()
-with (data_dir/"config.json").open('r') as f:
-    config=json.load(f)
+with (data_dir / "config.json").open('r') as f:
+    config = json.load(f)
 partitions, schema = None, None
 index_labels = []
+
 
 class Column(BaseModel):
     field: str
@@ -22,38 +25,44 @@ class Column(BaseModel):
     type: Optional[str]
     weight: Optional[float]
 
+
 class Schema(BaseModel):
     metric: str
     filters: List[Column]
     encoders: List[Column]
+
     def to_dict(self):
         return {
-        "metric": self.metric,
-        "filters": [vars(c) for c in self.filters],
-        "encoders": [vars(c) for c in self.encoders],
+            "metric": self.metric,
+            "filters": [vars(c) for c in self.filters],
+            "encoders": [vars(c) for c in self.encoders],
         }
 
+
 class KnnQuery(BaseModel):
-	data: Dict[str,str]
-	k:int
+    data: Dict[str, str]
+    k: int
 
 
 @app.get("/")
 async def read_root():
     return {"status": "OK", "schema_initialized": schema is not None}
 
+
 @app.get("/partitions")
 async def api_partitions():
     if schema is None:
         return {"status": "error", "message": "Schema not initialized"}
-    return {"status":"OK", "partitons": schema["partitions"], "n":len(schema["partitions"])}
+    return {"status": "OK", "partitons": schema["partitions"], "n": len(schema["partitions"])}
+
 
 @app.post("/encode")
-async def api_encode(data: Dict[str,str]):
+async def api_encode(data: Dict[str, str]):
     if schema is None:
         return {"status": "error", "message": "Schema not initialized"}
     vec = schema["encode_fn"](data)
     return {"status": "OK", "vec": [float(x) for x in vec]}
+
 
 @app.post("/init_schema")
 def init_schema(sr: Schema):
@@ -61,31 +70,34 @@ def init_schema(sr: Schema):
     schema = encoders.parse_schema(sr.to_dict())
     partitions = [LazyHnsw(schema["metric"], schema["dim"], **config["hnswlib"]) for _ in schema["partitions"]]
     # for index in partitions:
-        # index.init_index(**config["hnswlib"])
+    # index.init_index(**config["hnswlib"])
     return {"status": "OK"}
 
+
 @app.post("/index")
-async def api_index(data: List[Dict[str,str]]):
+async def api_index(data: List[Dict[str, str]]):
     if schema is None:
         return {"status": "error", "message": "Schema not initialized"}
     try:
-        vecs = sorted([(schema["index_num"](datum),schema["encode_fn"](datum), datum["id"]) for datum in data], key=at(0))
+        vecs = sorted([(schema["index_num"](datum), schema["encode_fn"](datum), datum["id"]) for datum in data],
+                      key=at(0))
     except KeyError as e:
         return {"status": "error", "message": str(e)}
     affected_partitions = 0
     labels = set(index_labels)
     for idx, grp in itertools.groupby(vecs, at(0)):
-        _,items,ids = zip(*grp)
+        _, items, ids = zip(*grp)
         for id in ids:
             if id not in labels:
                 labels.add(id)
                 index_labels.append(id)
-        if (partitions[idx].max_elements<len(items)):
+        if (partitions[idx].max_elements < len(items)):
             partitions[idx].resize_index(len(items))
-        affected_partitions+=1
+        affected_partitions += 1
         num_ids = list(map(index_labels.index, ids))
         partitions[idx].add_items(items, num_ids)
     return {"status": "OK", "affected_partitions": affected_partitions}
+
 
 @app.post("/query")
 async def api_query(query: KnnQuery):
@@ -98,21 +110,20 @@ async def api_query(query: KnnQuery):
     try:
         vec = schema["encode_fn"](query.data)
     except Exception as e:
-        return {"status": "error", "message": "Error in encoding: " +  str(e)}
+        return {"status": "error", "message": "Error in encoding: " + str(e)}
     try:
-        labels, distances = partitions[idx].knn_query(vec, k = query.k)
+        num_ids, distances = partitions[idx].knn_query(vec, k=query.k)
     except Exception as e:
-        return {"status": "error", "message": "Error in querying: " +  str(e)}
-    ids,distances=[],[]
-    if len(ids)>0:
-        ids = [index_labels[l] for l in labels[0]]
+        return {"status": "error", "message": "Error in querying: " + str(e)}
+    if len(num_ids) == 0:
+        labels, distances = [], []
+    else:
+        labels = [index_labels[n] for n in num_ids[0]]
         distances = [float(d) for d in distances[0]]
-    return {"status":"OK", "ids": ids, "distances": distances}
+    return {"status": "OK", "ids": labels, "distances": distances}
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("__main__:app", host="0.0.0.0", port=5000, log_level="info")
 
+    uvicorn.run("__main__:app", host="0.0.0.0", port=5000, log_level="info")
