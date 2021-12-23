034681ed95236b3306532da6c67e7ec1f8b73dd0
Uri Goren <uri@goren.ml>
2021-12-23 17:29:19 +0200

    repartitioning

--- src/repartition.py
+++ src/repartition.py
@@ -1,12 +1,17 @@
-import os, json, collections
+import json, collections, logging
 import requests
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from encoders import parse_schema
-from multiprocessing import Pool
+from pathlib import Path
+from joblib import delayed, Parallel
 
-with open("ny_schema.json",'r') as f:
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+data_dir = Path("../data/ny/").absolute()
+REPARTITON_LIMIT=10000
+
+with open(data_dir / "schema.json",'r') as f:
     schema = json.load(f)
 partiton_field = schema["filters"][0]["field"]
 partiton_values = schema["filters"][0]["values"]
@@ -15,33 +20,42 @@ encode = parse_schema(schema)["encode_fn"]
 
 partitions = collections.defaultdict(pd.DataFrame)
 
-print("repartition")
-p_files = [f for f in os.listdir() if f.endswith(".parquet")]
-j_files = []
-for f in tqdm(p_files):
-    df = pd.read_parquet(f)
-    for v in partiton_values:
-        partitions[v]=pd.concat([partitions[v],df[df[partiton_field]==v]])
+logging.debug("repartition")
+p_files = [f for f in data_dir.glob("*.parquet")]
+for v in tqdm(partiton_values):
+    subindex = 0
+    for f in p_files:
+        df = pd.read_parquet(f)
+        df=df[df[partiton_field]==v]
+        n = len(df)//REPARTITON_LIMIT
+        for sp in range(n+1):
+            increment_df = df.iloc[:REPARTITON_LIMIT]
+            if len(partitions[f"{v}_{subindex}"])+len(increment_df)>REPARTITON_LIMIT:
+                subindex+=1
+            partitions[f"{v}_{subindex}"]=pd.concat([partitions[v],increment_df])
+            df=df.iloc[REPARTITON_LIMIT:]
+            if len(increment_df)>0:
+                subindex+=1
 
-print("save to local json files")
+logging.debug("save to local json files")
+(data_dir/"partitioned").mkdir(exist_ok=True)
+j_files = []
 for p,df in partitions.items():
-    j_file = "paritioned/"+p+".json"
+    j_file = data_dir/"partitioned"/(p+".json")
     df.to_json(j_file, orient='records')
     j_files.append(j_file)
 
-print("encoding each partition")
-#TODO: run in parallel
-# pool = Pool(8)
-# def encode_one_partition(jf):
-    # with open(jf,'r') as f:
-        # data = json.load(f)
-    # arr = np.vstack([encode(datum) for datum in data])
-    # np.save(jf.replace(".json", ".npy"), arr)
-# pool.map(encode_one_partition,j_files)
+logging.debug("encoding each partition")
+def fstack(f,lst):
+    """Combines np.vstack and parallelism"""
+    if len(lst)==0:
+        return np.array([])
+    elif len(lst)<1024:
+        return np.vstack(Parallel(-1)([f(d) for d in lst]))
+    return np.vstack([fstack(f,lst[:len(lst)//2]),fstack(f,lst[len(lst)//2:])])
 
 for jf in tqdm(j_files):
-    print(jf)
     with open(jf,'r') as f:
         data = json.load(f)
-    arr = np.vstack([encode(datum) for datum in data])
-    np.save(jf.replace(".json", ".npy"), arr)
+    arr = fstack(delayed(encode), data)
+    np.save(str(jf).replace(".json", ".npy"), arr)
