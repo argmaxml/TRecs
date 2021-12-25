682e933445a2871926f1bb24a1596bc023b29b9c
Uri Goren <uri@goren.ml>
2021-12-25 20:30:26 +0200

    supporting multiple filters

--- src/repartition.py
+++ src/repartition.py
@@ -11,7 +11,7 @@ from joblib import delayed, Parallel
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 data_dir = Path(__file__).absolute().parent.parent /"data/ny"
 REPARTITON_LIMIT=10000
-PARTITION_SEP='_'
+PARTITION_SEP='~'
 
 with open(data_dir / "schema.json",'r') as f:
     schema = json.load(f)
@@ -22,7 +22,6 @@ with (partition_dir/"schema.json").open('w') as f:
     json.dump(schema, f)
 #
 partiton_field = schema["filters"][0]["field"]
-partiton_values = schema["filters"][0]["values"]
 
 schema = parse_schema(schema)
 encode = schema["encode_fn"]
@@ -31,17 +30,19 @@ partitions = collections.defaultdict(pd.DataFrame)
 
 logging.debug("Repartition")
 p_files = [f for f in data_dir.glob("*.parquet")]
-for v in tqdm(partiton_values):
+for partition in tqdm(schema["partitions"]):
+    kv=list(zip(schema["filters"],partition))
     subindex = 0
     for f in p_files:
         df = pd.read_parquet(f)
-        df=df[df[partiton_field]==v]
+        for k,v in kv:
+            df=df[df[k]==v]
         n = len(df)//REPARTITON_LIMIT
         for sp in range(n+1):
             increment_df = df.iloc[:REPARTITON_LIMIT]
-            if len(partitions[f"{v}{PARTITION_SEP}{subindex:05d}"])+len(increment_df)>REPARTITON_LIMIT:
+            if len(partitions[partition+(subindex,)])+len(increment_df)>REPARTITON_LIMIT:
                 subindex+=1
-            partitions[f"{v}{PARTITION_SEP}{subindex:05d}"]=pd.concat([partitions[f"{v}{PARTITION_SEP}{subindex:05d}"],increment_df])
+            partitions[partition+(subindex,)]=pd.concat([partitions[partition+(subindex,)],increment_df])
             df=df.iloc[REPARTITON_LIMIT:]
             if len(increment_df)>0:
                 subindex+=1
@@ -49,8 +50,9 @@ for v in tqdm(partiton_values):
 logging.debug("Save to local json files")
 # save jsons
 j_files = []
-for p,df in partitions.items():
-    j_file = partition_dir/(p+".json")
+for partition,df in partitions.items():
+    p = PARTITION_SEP.join(map(str,partition))
+    j_file = partition_dir/(p)
     df.to_json(j_file, orient='records')
     j_files.append(j_file)
 
@@ -65,19 +67,21 @@ def fstack(f,lst):
 
 start_num_idx=0
 for jf in tqdm(j_files):
-    part_name, part_num = jf.name.rsplit(PARTITION_SEP,1)
+    partition = jf.name.split(PARTITION_SEP)
+    subindex = int(partition[-1])
+    partition = partition[:-1]
     with open(jf,'r') as f:
         data = json.load(f)
     arr = fstack(delayed(encode), data)
     np.save(jf.with_suffix('.npy'), arr)
     with jf.with_suffix('.meta').open('w') as f:
-        index_num = schema["index_num"]({partiton_field:part_name})
+        index_num = schema["index_num"](dict(zip(schema["filters"],partition)))
         meta = {
             "size":len(arr),
             "start_num_idx":start_num_idx,
             "ids": [d['id'] for d in data],
-            "partition_name": part_name,
-            "partition_number":part_num,
+            "partition": partition,
+            "subindex": subindex,
             "index_num":index_num,
             "metric":schema["metric"],
             "dim":schema["dim"],
